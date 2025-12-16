5c4aa9c3b8a0265598d2f6799fb9589a8717e44a
LGenSOC <149721085+LGenSOC@users.noreply.github.com>
2025-12-16 14:04:52 +0100

    adding files and data layer

---
+++ products.js
@@ -0,0 +1,8 @@
+export const productsData = [
+    { name: "Lightweight Alpine Running Jacket", imageUrl: "https://cdn.shopify.com/s/files/1/0027/3953/5981/products/wj04-white_main.jpg?v=1555602007", price: "€45.00", link: "https://demo.dynamic-yield.com/products/wj04" },
+    { name: "Printed Yoga Jacket", imageUrl: "https://cdn.shopify.com/s/files/1/0027/3953/5981/products/wj09-green_main.jpg?v=1555602017", price: "€25.00", link: "https://demo.dynamic-yield.com/products/wj09" },
+    { name: "Comfy Sports Jacket", imageUrl: "https://cdn.shopify.com/s/files/1/0027/3953/5981/products/wj10-yellow_main.jpg?v=1555602122", price: "€40.00", link: "https://demo.dynamic-yield.com/products/wj10" },
+    { name: "Long sleeved Purple Jacket", imageUrl: "https://cdn.shopify.com/s/files/1/0027/3953/5981/products/wh09-purple_main.jpg?v=1555601769", price: "€25.00", link: "https://demo.dynamic-yield.com/products/wh09" },
+    { name: "Sports Bra", imageUrl: "https://cdn.shopify.com/s/files/1/0027/3953/5981/products/wb03-green_main.jpg?v=1555601852", price: "€15.00", link: "https://demo.dynamic-yield.com/products/wb03" },
+    { name: "Hoodie Jacket", imageUrl: "https://cdn.shopify.com/s/files/1/0027/3953/5981/products/wh08-white_main.jpg?v=1555601848", price: "€60.00", link: "https://demo.dynamic-yield.com/products/wh08" },
+];
